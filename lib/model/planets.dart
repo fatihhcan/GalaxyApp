89ebb795d5be9b6abc152bee0ee56266ef4cd2b8
Fatih Can <ybsfatihcan@gmail.com>
2020-09-01 17:15:07 +0300

    Model yapısı oluşturuldu.

---
+++ lib/model/planets.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+List<Question> questionFromJson(String str) => List<Question>.from(json.decode(str).map((x) => Question.fromJson(x)));
+
+String questionToJson(List<Question> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Question {
+    Question({
+        this.id,
+        this.name,
+        this.description,
+    });
+
+    int id;
+    String name;
+    String description;
+
+    factory Question.fromJson(Map<String, dynamic> json) => Question(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+    };
+}
